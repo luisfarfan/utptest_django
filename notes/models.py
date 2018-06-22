060b452d5ebbbb54f6a541847f4d3149b16b0bbe
Luis Farfan <lucho.farfan9@gmail.com>
2018-06-22 12:04:32 -0500

    edit model

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
+
 from django.db import models
+
+from users.models import Users
 
 
 class Notes(models.Model):
@@ -7,4 +9,4 @@ class Notes(models.Model):
     description = models.CharField(max_length=255)
     date_create = models.DateTimeField(auto_now_add=True)
     date_edited= models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
